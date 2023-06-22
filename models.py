c90717c430006101f598665217f19ea6aba16611
nazarov <collermix@gmail.com>
2023-06-22 18:54:11 +0300

    Add UpdateUserRequest model

--- models.py
+++ models.py
@@ -4,15 +4,21 @@ from database import Base
 from pydantic import BaseModel
 
 
-# requests model
+# requests models
 class AddUserRequest(BaseModel):
     username: str
     email: str
 
 
-# postgres table
+class UpdateUserRequest(BaseModel):
+    user_id: str
+    username: str
+    email: str
+
+
+# postgres tables
 class UsersTable(Base):
     __tablename__ = "users"
     user_uuid = Column(UUID, primary_key=True, unique=True)
     username = Column(String)
-    email = Column(String, unique=True)
+    email = Column(String)
