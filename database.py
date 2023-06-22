09e2505f27aa828c9df046e182a2ce824c8bf7fd
nazarov <collermix@gmail.com>
2023-06-22 19:03:20 +0300

    Updated DATABASE_URL for docker

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/CRUD_task_DB"
+DATABASE_URL = "postgresql://postgres:postgres@postgres:5432/CRUD_task_DB"
 
 database = Database(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
