bd75dd52ec63f6efee0cd784a663daeda60d5d67
nazarov <collermix@gmail.com>
2023-06-22 17:19:03 +0300

    Set up models for request and DB table

---
+++ models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
+from database import Base
+from pydantic import BaseModel
+
+
+# requests model
+class AddUserRequest(BaseModel):
+    username: str
+    email: str
+
+
+# postgres table
+class UsersTable(Base):
+    __tablename__ = "users"
+    user_uuid = Column(UUID, primary_key=True, autoincrement=False)
+    username = Column(String)
+    email = Column(String)
+
