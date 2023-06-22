e8d1ee0f6b4d12c5b3793b8e5b4a0a025bff4992
nazarov <collermix@gmail.com>
2023-06-22 17:45:47 +0300

    Add handler "/add_user"

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import uuid
 import uvicorn
-from fastapi import FastAPI, Query, UploadFile, File, Form
+from fastapi import FastAPI
+from fastapi.responses import Response
+from models import UsersTable, AddUserRequest
 from database import database, engine, Base, SessionLocal
+from sqlalchemy.dialects.postgresql import insert
 
 # FastAPI init
 app = FastAPI()
@@ -23,6 +27,23 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+
+
+@app.post("/add_user", status_code=201)
+async def add_user(user_data: AddUserRequest):
+    user = {
+        "user_uuid": str(uuid.uuid4()),
+        "username": user_data.username,
+        "email": user_data.email
+    }
+
+    db = SessionLocal()
+    query = insert(UsersTable).values(**user)
+    db.execute(query)
+    db.commit()
+    db.close()
+
+    return {"message": "New user was added"}
 
 
 if __name__ == "__main__":
