0a42b598243db5a0fc88ed07226b632204f0d09c
nazarov <collermix@gmail.com>
2023-06-23 21:21:05 +0300

    Updated import declarative base

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 from databases import Database
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql://postgres:postgres@postgres:5432/CRUD_task_DB"
 
